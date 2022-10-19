6976640f95be7719fb5d36fcc797b5b9ee7fe6fa
thayour20 <suleimanridwan@gmail.com>
2022-10-19 10:08:49 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpha. ten times
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void);
+{
+	chal x = 0;
+	chal a;
+
+	while (x <= 10)
+
+	{
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+			x++;
+			_putchar('\n');
+		}
+	return (0);
+}
