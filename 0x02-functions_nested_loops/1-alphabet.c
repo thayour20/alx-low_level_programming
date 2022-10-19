605a65674b6ce2c4efe4bbfd390c8a87c71b6407
thayour20 <suleimanridwan@gmail.com>
2022-10-19 09:00:40 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabelt with char
+ * Return: always 0
+ */
+
+void print_alphabet(void);
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+
+
+	_putchar(alp);
+
+	_putchar('\n');
+
+}
