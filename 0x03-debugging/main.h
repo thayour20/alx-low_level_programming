1b78b7f2b7105f28cc7d24d313ebfc165f9a944b
thayour20 <suleimanridwan@gmail.com>
2022-10-20 10:12:37 -0700

    s

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 
-void positine_or_negtine(int i)
-
+void positine_or_negtine(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 
 #endif /* MAIN.H */
