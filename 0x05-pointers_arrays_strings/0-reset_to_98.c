067fc162abb40b06375c2b0ed3c8f14311e19e66
thayour20 <suleimanridwan@gmail.com>
2022-10-24 19:42:39 -0700

    proto

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98-Resets the value of a variable back to 98.
+ * @n:Variable to be reset.
+ * Return:Void.
+ */
+
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = &n;
+	*p = 98;
+
+	return (0);
+}
