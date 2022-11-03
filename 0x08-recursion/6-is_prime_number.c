0cc7066e46529b8631890399dbf775f3b50fb354
thayour20 <suleimanridwan@gmail.com>
2022-11-03 03:47:02 -0700

    task 5 and 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * is_prime_number - to check if a number is prime number
+ * @n: integer n
+ * Return: 1 if n is prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+		return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - to calculate if a prime is peime reculsive
+ * @n: number to evaluate
+ * @i: iterate
+ * Return: prime number
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+		return (actual_prime(n, i - 1));
+}
